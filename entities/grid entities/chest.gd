61cfb28f313ad5bf3fa6bdfac1edb2be220d7472
Alex-Boos <alexandra-berlin@web.de>
2024-10-14 16:01:45 +0200

    Insert animation for opening chest.

--- entities/grid entities/chest.gd
+++ entities/grid entities/chest.gd
@@ -1,5 +1,6 @@
 class_name Chest extends AnimatedSprite2D
 const POSSIBLE_CARDS_COUNT = 5
+var is_open = false
 
 var card: CardResource = [
 	CardKnightBasic,
@@ -10,7 +11,11 @@ var card: CardResource = [
 ].pick_random().new()
 
 func open():
-	Global.game_manager.players_cards.append(card)
-	Global.game_manager.selected_card = Global.game_manager.players_cards.back()
-	Global.game_manager.refresh_card_deck()
-	queue_free()
+	if is_open == false:
+		Global.game_manager.players_cards.append(card)
+		Global.game_manager.selected_card = Global.game_manager.players_cards.back()
+		Global.game_manager.refresh_card_deck()
+		self.play("open")
+		await animation_finished
+	else:
+		is_open = true
