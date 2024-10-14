33c3e98ac73c57ec826794104b9fc68821b7f30c
Alex-Boos <alexandra-berlin@web.de>
2024-10-14 17:51:25 +0200

    When an enemy is killed, a coin will now fly up to the money counter.

--- entities/enemies/base_enemy.gd
+++ entities/enemies/base_enemy.gd
@@ -12,6 +12,16 @@ func take_damage(_hp_loss:int):
 	hp -= 1
 	if hp <= 0:
 		SignalBus.enemy_died.emit(self)
+		# trigger coin animation before death of enemy
+		var coin = preload("res://items/coin_rotate.tscn").instantiate()
+		add_child(coin)
+		coin.play("coin_rotate")
+		# show rotating coin for 0.3 sec, then let it fly to the upper corner towards the money counter
+		await get_tree().create_timer(0.3).timeout
+		var target_pos: Vector2 = get_viewport().get_camera_2d().position - (get_viewport_rect().size / 2)
+		var coin_tween = create_tween()
+		coin_tween.tween_property(coin, "global_position", target_pos, 0.6)
+		await coin_tween.finished
 		queue_free()
 
 # selects to which file move or if attack
