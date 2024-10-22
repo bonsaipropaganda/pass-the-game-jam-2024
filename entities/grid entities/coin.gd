028730be7da896853538a667daef8b1f8401b19a
Khusheete <Siandfrance@gmail.com>
2024-10-22 12:14:31 +0200

    Added money sound on coin pickup

--- entities/grid entities/coin.gd
+++ entities/grid entities/coin.gd
@@ -6,5 +6,6 @@ func _ready():
 
 func _on_area_entered(area):
 	if area.is_in_group("player_area"):
+		AudioManager.sfx_play(AudioManager.sfx_enum.MONEY)
 		Global.game_manager.money += 1
 		queue_free()
