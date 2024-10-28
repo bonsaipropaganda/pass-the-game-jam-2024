b3a634a1a70d2718a50414290619cb9a7ff433f1
Nick "Kaiz <62921588+KaizNike@users.noreply.github.com>
2024-10-28 08:46:18 -0500

    Score Saving and Fun with BBCode

--- ui/menus/death_menu.gd
+++ ui/menus/death_menu.gd
@@ -5,7 +5,43 @@ extends Control
 
 func start():
 	$"UI/Main Menu/Labels/RoomsClearedLabel".text = "Rooms Cleared: " + str(Global.game_manager.rooms_cleared)
+	$"UI/Main Menu/Labels/BestScoreLabel".clear()
+	$"UI/Main Menu/Labels/BestScoreLabel".append_text(fun_with_dying_text())
 	$UI.visible = true
+
+## Death Menu's $"UI/Main Menu/Labels/BestScoreLabel" text change, in BBCode
+func fun_with_dying_text() -> String:
+	var text = ""
+	var save = Global.game_manager.load_scores()
+	print("scores should be there")
+	if not save:
+		return "- [color=yellow] NEW BEST!!! [/color] -"
+	var recent_score = Global.game_manager.rooms_cleared
+	
+	if check_if_highest(save.everyRunRoomsBeaten, recent_score):
+			return "- [color=yellow] NEW BEST!!! [/color] -"
+	else:
+		var rng = RandomNumberGenerator.new()
+		rng.randomize()
+		var rand = rng.randi() % 3 # Number of alternatives, fill em in.
+		match rand:
+			0:
+				return "alright."
+			1:
+				return "this is your " + str(save.everyRunRoomsBeaten.size() + 1) + " run"
+			2:
+				return "farewell, [color=red]MORTAL COIL[/color]"
+	return text
+
+
+func check_if_highest(arr:Array[int],score:int) -> bool:
+	var highest = 0
+	for run in arr:
+		if run > highest:
+			highest = run
+	if score > highest:
+		return true
+	return false
 
 func _on_main_menu_button_pressed() -> void:
 	AudioManager.music_transition_to(AudioManager.song_enum.GAMEPLAY_1, 3.0)
