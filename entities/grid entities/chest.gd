fa022ca942fe58515cc5dcb01f270b31b842eee2
Nick "Kaiz <62921588+KaizNike@users.noreply.github.com>
2024-10-28 11:44:08 -0500

    Princess Update
    About as powerful as a knight, perhaps promotes to a Queen?

--- entities/grid entities/chest.gd
+++ entities/grid entities/chest.gd
@@ -2,18 +2,20 @@ class_name Chest extends AnimatedSprite2D
 const POSSIBLE_CARDS_COUNT = 5
 var is_open = false
 
-var card: CardResource = [
+var card = [
 	CardKnightBasic,
 	CardPawnBasic,
 	CardBishopBasic,
 	CardRookBasic,
 	CardPlanB
-].pick_random().new()
+]
 
 func open():
 	if is_open == false:
 		is_open = true
-
+		if Global.game_manager.danger_level > 1:
+			card.append(CardPrincessBasic)
+		card = card.pick_random().new()
 		Global.game_manager.add_card(card)
 		$ChestSoundPlayer.play()
 
